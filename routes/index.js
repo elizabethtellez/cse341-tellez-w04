312c9fcb4f2af8b73b8c98fc598af4d56cd1e1ac
elizabethtellez <tel21004@byui.edu>
2023-01-26 22:23:07 -0800

    render changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/contacts', require('./contacts.js'))
+router.use('/contacts', require('./contacts.js'));
+router.use('/', require('./swagger.js'));
+
 
 module.exports = router;
